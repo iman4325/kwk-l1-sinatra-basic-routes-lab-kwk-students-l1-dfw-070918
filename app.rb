96e3f675238e7aac673e3f5c7dc0f77c781c02be
iman4325 <imantanzeem@hotmail.com>
2018-07-17 21:10:05 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,6 +2,12 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/name' do
-  "Hello, World!"
+  "Iman"
+end
+get '/hometown' do
+  "Southlake,TX"
+end
+get '/favorite-song' do
+  "My favorite song is In My Feelings "
 end
 end
